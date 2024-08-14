1e699d932653395d40495d49507af4ecf4d4efb0
Dev Jadav <v-dev.jadav@kfintech.com>
2024-08-14 22:18:07 +0530

    Updated enums and menuitems

---
+++ src/FXChatBot/Constants/MenuItems/menuItems.ts
@@ -0,0 +1,144 @@
+import { MenuItem } from "../../Interface/index.ts";
+
+export const FINANCIAL_ADVICE_MENUITEMS: MenuItem[] =[
+    {
+        "label": "Investment Plan",
+        "value": "Investment Plan",
+    },
+    {
+        "label": "Spends Advice",
+        "value": "Spends Advice",
+    },
+    {
+        "label": "Goal Planning",
+        "value": "Goal Planning",
+    },
+];
+
+export const INVESTMENT_CATEGORIES_MENUITEMS: MenuItem[] =[
+    {
+        "label": "Mutual Fund",
+        "value": "Mutual Fund",
+    },
+    // {
+    //     "label": "Stocks",
+    //     "value": "Stocks",
+    // },
+    // {
+    //     "label": "Gold",
+    //     "value": "Gold",
+    // },
+];
+
+export const MUTUAL_FUND_MENUITEMS: MenuItem[] =[
+    {
+        "label": "About MF",
+        "value": "About MF",
+    },
+    {
+        "label": "Top Performing Funds",
+        "value": "Top Performing Funds",
+    },
+    {
+        "label": "Filter Funds",
+        "value": "Filter Funds",
+    },
+    // {
+    //     "label": "Calculate Returns",
+    //     "value": "Calculate Returns",
+    // },
+    // {
+    //     "label": "Achieve Goal",
+    //     "value": "Achieve Goal",
+    // },
+];
+
+export const STOCKS_MENUITEMS: MenuItem[] =[
+    {
+        "label": "About Stocks",
+        "value": "About Stocks",
+    },
+    {
+        "label": "Top Performing Stocks",
+        "value": "Top Performing Stocks",
+    },
+    {
+        "label": "Filter Stocks",
+        "value": "Filter Stocks",
+    },
+    // {
+    //     "label": "Calculate Returns",
+    //     "value": "Calculate Returns",
+    // },
+    // {
+    //     "label": "Achieve Goal",
+    //     "value": "Achieve Goal",
+    // },
+];
+
+export const GOLD_MENUITEMS: MenuItem[] =[
+    {
+        "label": "About gold",
+        "value": "About gold",
+    },
+    {
+        "label": "Current Price of Gold",
+        "value": "Current Price of Gold",
+    },
+    {
+        "label": "Calculate Returns",
+        "value": "Calculate Returns",
+    },
+];
+
+export const RISK_RETURN: MenuItem[] =[
+    {
+        "label": "Risk",
+        "value": "Risk",
+    },
+    {
+        "label": "Return",
+        "value": "Return",
+    },
+];
+
+export const INVESTMENT_METHOD_MENUITEMS: MenuItem[] =[
+    {
+        "label": "SIP",
+        "value": "sip",
+    },
+    {
+        "label": "LUMPSUM",
+        "value": "lumpsum",
+    },
+];
+
+export const RISK_MENUITEM: MenuItem[] =[
+    {
+        "label": "Low",
+        "value": "Low",
+    },
+    {
+        "label": "Medium",
+        "value": "Medium",
+    },
+    {
+        "label": "High",
+        "value": "High",
+    },
+];
+
+export const RETURNS_MENUITEM: MenuItem[] =[
+    {
+        "label": "5-15%",
+        "value": "5-15%",
+    },
+    {
+        "label": "15-25%",
+        "value": "15-25%",
+    },
+    {
+        "label": "25+%",
+        "value": "25+%",
+    },
+];
